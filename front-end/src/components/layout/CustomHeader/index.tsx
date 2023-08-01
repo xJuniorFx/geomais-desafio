bacea130502c787dc0bb185327333cfd9ebbc140
Junior <juniorfernandessilva1@gmail.com>
2023-07-31 21:45:35 -0300

    Criando Cabeçalho

---
+++ front-end/src/components/layout/CustomHeader/index.tsx
@@ -0,0 +1,18 @@
+import { Col, Divider, Layout, Row } from 'antd';
+import React from 'react';
+import imagem from '../../../assets/logo-expanded.png'
+
+const { Header } = Layout;
+
+const CustomHeader: React.FC = () => {
+
+  return (
+    <Header color='dark' style={{ padding: '0' }}>
+      <Row justify="end">
+        <Col style={{padding: '5px 10px'}} span='md'><img src={imagem} alt="Logo Geomais" /></Col>
+      </Row>
+    </Header>
+  );
+};
+
+export default CustomHeader;
